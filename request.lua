a1da1786a9c7e49c223fc210b186b8e73485f322
Sean Pringle <sean.pringle@gmail.com>
2016-04-20 11:55:43 +1000

    test

---
+++ request.lua
@@ -0,0 +1,45 @@
+--[[
+Copyright (c) 2016 Sean Pringle sean.pringle@gmail.com
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+--]]
+
+work.submit("hello")
+print(work.collect())
+
+t = { hello = "world", "snaffle", 3.14, 1.0, { "what", "the", "heck?" } }
+s = table.json_encode(t)
+
+print(s)
+tt = table.json_decode(s)
+print(tt["4"][2])
+
+t = { "world", "snaffle", 3.14, 1.0, "what" }
+s = table.json_encode(t)
+
+error(s)
+
+for k,v in pairs(table.json_decode(s)) do
+  print(k.." => "..v)
+end
+
+
+print(1,2,3)
+
